f9e4301cb95412f68f1812bea996e8b2504b459a
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-12-25 17:32:43 +0900

    FEAT: 기본 스타일 설정, 폰트, 아이콘 추가

---
+++ src/styles/colors.js
@@ -0,0 +1,16 @@
+export const Colors = Object.freeze({
+  logo: '#CC0000',
+  brand: '#E60023',
+  brand2: '#AD081B',
+  black: '#121212',
+  success: '#0FA573',
+  warning: '#D32D2F',
+  facebook: '#176CF0',
+  blue: '#3172E0',
+  lightblue: '#9CC0FB',
+  grey: '#E6E6E6',
+  darkgrey: '#CDCDCD',
+  lightgrey: '#EFEFEF',
+  iconHover: '#F0F0F0',
+  white: '#FFFFFF',
+});
