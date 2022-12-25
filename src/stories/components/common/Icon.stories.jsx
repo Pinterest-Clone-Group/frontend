685f4dda7ad2603218c08682a3dcff4dc2068955
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-12-25 19:13:39 +0900

    FEAT: 재사용 icon들 컴포넌트로 추가
    props를 변경하면서 사용하고 싶을 수 있다.

---
+++ src/stories/components/common/Icon.stories.jsx
@@ -0,0 +1,23 @@
+import { Colors } from '../../../styles';
+import Icon from '../../../components/common/icons/Icon';
+
+export default {
+  title: 'components/common/Icons',
+  component: Icon,
+};
+
+export const Default = () => {
+  return (
+    <div style={{ display: 'flex', gridColumnGap: '16px', gridRowGap: '16px' }}>
+      <Icon.Alert fill={Colors.darkgrey} />
+      <Icon.Close />
+      <Icon.CopyLink />
+      <Icon.Download />
+      <Icon.Like />
+      <Icon.Message />
+      <Icon.More />
+      <Icon.Search />
+      <Icon.ValidationWarning />
+    </div>
+  );
+};
