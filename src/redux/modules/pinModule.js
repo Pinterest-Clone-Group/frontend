4da4faac59f5371343210c26634f23edfa096564
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-12-28 08:58:48 +0900

    FEAT: 핀 상세보기 기능 추가

--- src/redux/modules/pinModule.js
+++ src/redux/modules/pinModule.js
@@ -1,5 +1,7 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
+import pinApi from '../../apis/pinApi';
+
 const initialState = {
   pins: [],
   pin: null,
@@ -12,6 +14,15 @@ export const __getPinList = createAsyncThunk('getPinList', async (payload, thunk
   try {
     const { data } = await payload.api();
     return thunkAPI.fulfillWithValue(data.pins);
+  } catch (err) {
+    return thunkAPI.rejectWithValue(err);
+  }
+});
+
+export const __getPinDetailById = createAsyncThunk('getPinDetailById', async (payload, thunkAPI) => {
+  try {
+    const { data } = await pinApi.getById(payload.id);
+    return thunkAPI.fulfillWithValue(data.pin);
   } catch (err) {
     return thunkAPI.rejectWithValue(err);
   }
@@ -30,6 +41,17 @@ const pinModule = createSlice({
       state.pins = action.payload;
     },
     [__getPinList.rejected]: (state, action) => {
+      state.isLoading = false;
+      state.error = action.payload;
+    },
+    [__getPinDetailById.pending]: (state) => {
+      state.isLoading = true;
+    },
+    [__getPinDetailById.fulfilled]: (state, action) => {
+      state.isLoading = false;
+      state.pin = action.payload;
+    },
+    [__getPinDetailById.rejected]: (state, action) => {
       state.isLoading = false;
       state.error = action.payload;
     },
