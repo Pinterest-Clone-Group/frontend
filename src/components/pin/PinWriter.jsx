22184b2fc1037b23da04f21c3f2e33e1c5991f4e
pds0309 <76927397+pds0309@users.noreply.github.com>
2022-12-29 14:56:13 +0900

    FIX: 댓글 등록 명세 변경 반영

--- src/components/pin/PinWriter.jsx
+++ src/components/pin/PinWriter.jsx
@@ -1,15 +1,22 @@
 import Button from '../common/Button';
 import ProfileImage from '../common/ProfileImage';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router';
 
 // TODO: 현재 이용자의 글쓴이 팔로우 상태에 따라 팔로우/언팔로우  버튼이 활성화된다.
 const PinWriter = ({ pin }) => {
+  const navigate = useNavigate();
+
   return (
     <PinWriterLayout>
       <WriterBox>
-        <ProfileImage size={50} imageUrl={pin.userImage} />
+        <div onClick={() => navigate('/users/' + pin.userId)}>
+          <ProfileImage size={50} imageUrl={pin.userImage} />
+        </div>
         <WriterInfoBox>
-          <p style={{ fontWeight: '600' }}>{pin.name}</p>
+          <WriterParagraph style={{ fontWeight: '600' }} onClick={() => navigate('/users/' + pin.userId)}>
+            {pin.name}
+          </WriterParagraph>
           <p style={{ opacity: 0.7 }}>팔로워 1,555명</p>
         </WriterInfoBox>
       </WriterBox>
@@ -30,6 +37,9 @@ const WriterBox = styled.div`
   display: flex;
   align-items: center;
   grid-column-gap: 8px;
+  & > div > div > img {
+    cursor: pointer;
+  }
 `;
 
 const WriterInfoBox = styled.div`
@@ -38,6 +48,13 @@ const WriterInfoBox = styled.div`
     font-size: 14px;
   }
   margin-bottom: 6px;
+`;
+
+const WriterParagraph = styled.p`
+  cursor: pointer;
+  :hover {
+    opacity: 0.8;
+  }
 `;
 
 const FollwerButtonBox = styled.div``;
