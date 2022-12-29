694cf4372f510a5c746f24d387a2dcca757641fa
ptg0811 <ptg0811@gmail.com>
2022-12-29 13:11:42 +0900

    FEAT: 회원정보, 프로필사진 수정 기능 완료/#27

--- src/components/header/signupModal/SignupOauthButtonSetBox.jsx
+++ src/components/header/signupModal/SignupOauthButtonSetBox.jsx
@@ -4,12 +4,19 @@ import styled from 'styled-components';
 import Button from '../../common/Button';
 
 const SignupOauthButtonSetBox = () => {
+  const redirectUri = process.env.REACT_APP_REDIRECT_URI;
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+
+  const kakaoSignup = () => {
+    window.location.href = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}`;
+  };
+
   const signupButton = { padding: '0px 18px', margin: '8px 0px 0px', height: '40px', width: '268px' };
 
   return (
     <OauthButtonSetBox>
-      <Button btnColor="facebook" style={signupButton}>
-        Facebook으로 계속하기
+      <Button btnColor="kakao" style={signupButton} onClick={kakaoSignup}>
+        Kakao talk으로 계속하기
       </Button>
       <Button btnColor="white" style={signupButton}>
         Google으로 계속하기
